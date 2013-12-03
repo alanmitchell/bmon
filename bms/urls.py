b795e9981b95c2f158e9a8d295b67619ee35b3a0
Alan Mitchell <tabb99@gmail.com>
2013-12-02 17:48:18 -0900

    Added main Django project files

---
+++ bms/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'rm.views.home', name='home'),
+    # url(r'^rm/', include('rm.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('bmsapp.urls')),
+)
