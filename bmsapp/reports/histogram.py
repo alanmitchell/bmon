23cb6e5c8a1384d4dc1196bff441bf09d247b5d0
alaskamapscience <github@alaskamapscience.com>
2015-01-08 12:37:08 -0900

    Added schedule information to histogram chart

--- bmsapp/reports/histogram.py
+++ bmsapp/reports/histogram.py
@@ -1,6 +1,9 @@
-import pandas as pd, numpy as np
-import bmsapp.models, bmsapp.data_util
+import pandas as pd
+import numpy as np
+import bmsapp.models
+import bmsapp.data_util
 import basechart
+
 
 class Histogram(basechart.BaseChart):
     """Class that creates Histogram plot.
@@ -14,38 +17,57 @@ class Histogram(basechart.BaseChart):
         # determine the sensor to plot from the sensor selected by the user.
         the_sensor = bmsapp.models.Sensor.objects.get(pk=self.request_params['select_sensor'])
 
+        # get the requested averaging interval in hours
+        averaging_hours = float(self.request_params['averaging_time'])
+
         # determine the start time for selecting records and loop through the selected
         # records to get the needed dataset
         st_ts, end_ts = self.get_ts_range()
         db_recs = self.reading_db.rowsForOneID(the_sensor.sensor_id, st_ts, end_ts)
+
         # extract out the values and the timestamps into numpy arrays.
         values = np.array([rec['val'] for rec in db_recs])
         times = np.array([rec['ts'] for rec in db_recs])
         if len(values):
-            # create a Pandas Time Series to allow easy time averaging
+            # create a Pandas Time Series
             pd_ser = pd.Series(values, index=times)
-            series = []   # will hold all the series created
-            # info needed to create each series (time averaging function, series name)
-            series_info = ( (None, 'Raw'),
-                            (bmsapp.data_util.TsBin(1).bin, '1 Hr Averages'),
-                            (bmsapp.data_util.TsBin(2).bin, '2 Hr Avg'),
-                            (bmsapp.data_util.TsBin(4).bin, '4 Hr Avg'),
-                            (bmsapp.data_util.TsBin(8).bin, '8 Hr Avg'),
-                            (bmsapp.data_util.TsBin(24).bin, '1 Day Avg') )
-            for avg_func, series_name in series_info:
-                if avg_func:
-                    avg_series = pd_ser.groupby(avg_func).mean()
+
+            chart_series = []   # will hold all the series created
+
+            # info needed to create each series (selection list, series name, visible)
+            if self.schedule:
+                occupied_times = []
+                unoccupied_times = []
+                for ts in pd_ser.index:
+                    if self.schedule.is_occupied(ts):
+                        occupied_times.append(ts)
+                    else:
+                        unoccupied_times.append(ts)
+
+                series_info = ((None, 'All Data', False),
+                               (occupied_times, 'Occupied Periods', True),
+                               (unoccupied_times, 'Unoccupied Periods', True))
+            else:
+                # no schedule, so just return the 'All Data' series
+                series_info = (None, 'All Data', True)
+
+            for select_list, series_name, visibility in series_info:
+                if select_list:
+                    select_series = pd_ser[select_list]
                 else:
-                    avg_series = pd_ser
-                one_series = {'data': bmsapp.data_util.histogram_from_series(avg_series), 'name': series_name}
-                if series_name != 'Raw':
-                    one_series['visible'] = False
-                series.append( one_series )
+                    select_series = pd_ser
+
+                if averaging_hours:
+                    select_series = select_series.groupby(bmsapp.data_util.TsBin(averaging_hours).bin).mean()
+
+                chart_series.append({'data': bmsapp.data_util.histogram_from_series(select_series),
+                                     'name': series_name,
+                                     'visible': visibility})
         else:
-            series = []
+            chart_series = []
 
         opt = self.get_chart_options()
-        opt['series'] = series
+        opt['series'] = chart_series
         opt['yAxis']['title']['text'] = '% of Readings'
         opt['yAxis']['min'] = 0
         opt['xAxis']['title']['text'] = the_sensor.unit.label
