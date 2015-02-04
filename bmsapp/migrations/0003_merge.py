04ac9076065b96d080ba6decd8bc16b6e41a84b9
Alan Mitchell <tabb99@gmail.com>
2015-02-04 18:01:39 +0000

    Merged two migrations

---
+++ bmsapp/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bmsapp', '0002_sensor_formatting_function'),
+        ('bmsapp', '0002_auto_20150203_2006'),
+    ]
+
+    operations = [
+    ]
