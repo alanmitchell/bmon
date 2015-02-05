9a9f9ef1be2d7e8f88b35d96297dfd205976f798
Alan Mitchell <tabb99@gmail.com>
2015-02-05 09:13:27 -0900

    Fixed Problem appending performance stats.

--- bmsapp/views.py
+++ bmsapp/views.py
@@ -93,7 +93,7 @@ def get_report_results(request):
         dbsize = os.path.getsize(chart_obj.reading_db.db_fname)
         loads = subprocess.check_output('uptime').split(':')[-1].split(',')
         loads = [float(ld) for ld in loads]
-        result['objects'] = result['objects'].append( ('stats', {'dbsize': dbsize, 'loads': loads}) )
+        result['objects'].append( ('stats', {'dbsize': dbsize, 'loads': loads}) )
     
     except:
         _logger.exception('Error in get_report_results')
