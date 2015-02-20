8cb29214c96d8d0c25cb8dd775d61874feed52d4
Alan Mitchell <tabb99@gmail.com>
2015-02-19 16:09:54 -0900

    Added one main cron file to dispatch all cron scripts

---
+++ bmsapp/scripts/main_cron.py
@@ -0,0 +1,50 @@
+'''This file is run by a CRON job every 5 minutes and dispatches the various
+cron tasks.  It should be run using the django-extensions runscript facility:
+
+    manage.py runscript main_cron
+
+'''
+
+from datetime import datetime
+import calc_readings
+import daily_status
+import backup_readingdb
+
+
+def suppress_errors(func):
+    '''Runs the function 'func' and suppresses all errors.
+    '''
+    try:
+        return func()
+    except:
+        return None
+
+
+def run():
+    '''The function executed by runscript.
+    '''
+
+    # current time
+    now = datetime.now()
+
+    # determine some time values that are needed for determining which
+    # cron jobs to run.
+    # day of the year, 1-366
+    yr_day = now.timetuple().tm_yday
+    # hour of the day, 0-23
+    hr = now.hour
+    # which 5 minute period within in the hour, 0-11
+    hr_div = int(now.minute / 5)
+
+    # at 15 and 45 minute marks in hour (roughly), run the calculate readings
+    # script
+    if hr_div in (3, 9):     
+        suppress_errors(calc_readings.run)
+
+    # run the daily status script 5 minutes after midnight each day
+    if hr==0 and hr_div==1:
+        suppress_errors(daily_status.run)
+
+    # run the sensor reading database backup every 3 days
+    if (yr_day % 3)==0 and hr==2 and hr_div==6:
+        suppress_errors(backup_readingdb.run)
