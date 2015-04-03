2a726157e38fc726ddaf4eaea514380fc98c9cdb
alaskamapscience <github@alaskamapscience.com>
2015-04-03 11:31:36 -0800

    Added star column to CurrentValuesMulti

--- bmsapp/reports/currentvalues_multi.py
+++ bmsapp/reports/currentvalues_multi.py
@@ -18,7 +18,12 @@ class CurrentValuesMulti(basechart.BaseChart):
         building_group_id = int(self.request_params['select_group'])
         buildings = bmsapp.view_util.buildings_for_group(building_group_id)
 
+        # create a blank list to store the sensor values for the report
         sensor_list = []
+
+        # create a blank dictionary to store value ranges for each unit
+        # the dictionary is keyed on unit_label, values are dictionaries with 'min' and 'max' keys
+        value_ranges = {}
 
         # loop through the selected buildings
         for bldg_info in self.chart_info.chartbuildinginfo_set.filter(building__in=buildings):
@@ -45,12 +50,25 @@ class CurrentValuesMulti(basechart.BaseChart):
                     cur_value = format_function(last_read['val']) if last_read else ''
                 else:
                     cur_value = bmsapp.data_util.formatCurVal(last_read['val']) if last_read else ''
+
                 minutes_ago = '%.1f' % ((cur_time - last_read['ts'])/60.0) if last_read else ''
-                cur_group_sensor_list.append( {'title': b_to_sen.sensor.title,
-                                               'cur_value': cur_value,
-                                               'unit': b_to_sen.sensor.unit.label,
-                                               'minutes_ago': minutes_ago,
-                                               'sensor_id': b_to_sen.sensor.id} )
+
+                # update the values in the value_ranges dictionary
+                if b_to_sen.sensor.unit.label in value_ranges:
+                    unit_range = value_ranges[b_to_sen.sensor.unit.label]
+                    unit_range['min'] = min(unit_range['min'], last_read['val'])
+                    unit_range['max'] = max(unit_range['max'], last_read['val'])
+                else:
+                    unit_range = {'min': last_read['val'], 'max': last_read['val']}
+                value_ranges[b_to_sen.sensor.unit.label] = unit_range
+
+                cur_group_sensor_list.append({'title': b_to_sen.sensor.title,
+                                              'cur_value': cur_value,
+                                              'last_read': last_read['val'],
+                                              'unit': b_to_sen.sensor.unit.label,
+                                              'minutes_ago': minutes_ago,
+                                              'sensor_id': b_to_sen.sensor.id,
+                                              'alerts': b_to_sen.sensor.alerts(self.reading_db)})
             # add the last group
             if cur_group:
                 building_sensor_list.append( (cur_group, cur_group_sensor_list) )
@@ -60,19 +78,31 @@ class CurrentValuesMulti(basechart.BaseChart):
                                 'group_sensor_list': building_sensor_list
                                 })
 
+        # add the percentage info for sensors
+        for building_info in sensor_list:
+            for group in building_info['group_sensor_list']:
+                for sensor in group[1]:
+                    value_range = value_ranges[sensor['unit']]
+                    if value_range['max'] != value_range['min']:
+                        last = sensor['last_read'] - value_range['min']
+                        high = value_range['max'] - value_range['min']
+                        sensor['pct'] = last / high
+                        sensor['stars'] = '*' * (int((last / high) * 9) + 1)
+                    else:
+                        sensor['pct'] = 0
+                        sensor['stars'] = ''
+
+
         # context for template
         context = Context({})
+
+        # create a report title
+        context['report_title'] = self.chart_info.title
 
         # make this sensor list available to the template
         context['sensor_list'] = sensor_list
 
-        # create a report title
-        context['report_title'] = "Current Values"
-
-        # template needs building ID.
-        context['bldg_id'] = self.bldg_id
-
-        # Get the ID of the first time series chart for this building
+        # Get the ID of the first time series chart for a building
         context['ts_chart_id'] = basechart.TIME_SERIES_CHART_ID
 
         template = loader.get_template('bmsapp/CurrentValuesMulti.html')
