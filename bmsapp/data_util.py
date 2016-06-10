aa4d73f8496d2613e5c64f1dc0590cc1000a2fbe
Alan Mitchell <tabb99@gmail.com>
2016-06-10 11:33:03 -0800

    Added 2 week, 1 month, & 1 year time averaging.

--- bmsapp/data_util.py
+++ bmsapp/data_util.py
@@ -65,7 +65,8 @@ def formatCurVal(val):
         return '{:,}'.format( int(float('%.3g' % val)))
     else:
         return '%.3g' % val
-    
+
+
 class TsBin:
     '''
     Class to determine a timestamp bin value (UNIX seconds) for purposes of time-averaging
@@ -75,21 +76,54 @@ class TsBin:
 
     def __init__(self, bin_width, tz=default_tz):
         '''
-        'bin_width' is the bin width in hours.
+        'bin_width' is the bin width in hours.  The values of 720 and 8760 are
+        treated special.  The value 720 indicates that binning by month is
+        requested, and actual month boundaries are used to determine the bins
+        instead of bins exactly 720 hours wide.  A value of 8760 indicates
+        that yearly bins are requested, and bins will be on exactly year
+        boundaries instead of exactly 8760 hours wide.
         '''
+
+        # save bin width in hours and in seconds
+        self.bin_width = bin_width
         self.bin_wid_secs = bin_width * 3600.0  # bin width in seconds
+
+        self.tz = tz  # save timezone
 
         # determine a reference timestamp that occurs at the start of a bin boundary for all
         # binning widths.  That would be a Monday at 0:00 am.
-        ref_dt = tz.localize(datetime(2013, 1, 7))
-        self.ref_ts = time.mktime(ref_dt.timetuple())
+        ref_dt = tz.localize(datetime(2000, 1, 3))
+        self.ref_ts = calendar.timegm(ref_dt.utctimetuple())
 
     def bin(self, ts):
         '''
         Returns the bin midpoint for 'ts' in Unix seconds.
+
+        Month and Year binning are slow (~17 us per timestamp) and could be sped
+        up by creating a dictionary that maps each day to a bin timestamp.  The
+        dictionary keys would span a range of all likely timestamps and would be
+        created in the __init__ constructor. The timestamp passed into this bin
+        routine would first be moved to the start of the day to conform with the
+        dictionary keys.
         '''
-        bin_int = int((ts - self.ref_ts) / self.bin_wid_secs)
-        return bin_int * self.bin_wid_secs + self.bin_wid_secs * 0.5 + self.ref_ts
+        if self.bin_width == 720:
+            # month binning. Put it into bin in middle of month.
+            dt = datetime.fromtimestamp(ts, self.tz).replace(day=16, hour=0, minute=0, second=0, microsecond=0)
+            # this may still be expressed in Standard time instead of Daylight Savings time, so correct
+            dt = self.tz.normalize(dt).replace(hour=0)
+            return calendar.timegm(dt.utctimetuple())
+
+        if self.bin_width == 8760:
+            # year binning.  Bin is middle of year (roughly)
+            dt = datetime.fromtimestamp(ts, self.tz).replace(month=7, day=1, hour=0, minute=0, second=0, microsecond=0)
+            # this may still be expressed in Standard time instead of Daylight Savings time, so correct
+            dt = self.tz.normalize(dt).replace(hour=0)
+            return calendar.timegm(dt.utctimetuple())
+
+        else:
+            bin_int = int((ts - self.ref_ts) / self.bin_wid_secs)
+            return bin_int * self.bin_wid_secs + self.bin_wid_secs * 0.5 + self.ref_ts
+
 
 def histogram_from_series(pandas_series):
     '''
