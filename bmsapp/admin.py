2cea3533f8b5293d4411101fabd1a582f3c440a2
Alan Mitchell <tabb99@gmail.com>
2016-10-29 13:51:18 -0800

    Made Admin Interface for Periodic Script.

--- bmsapp/admin.py
+++ bmsapp/admin.py
@@ -5,7 +5,7 @@ This file configures the Admin interface, which allows for editing of the Models
 from bmsapp.models import Building, Sensor, SensorGroup, BldgToSensor, DashboardItem, Unit
 from bmsapp.models import MultiBuildingChart, ChartBuildingInfo
 from bmsapp.models import BuildingGroup, BuildingMode
-from bmsapp.models import AlertCondition, AlertRecipient
+from bmsapp.models import AlertCondition, AlertRecipient, PeriodicScript
 from django.contrib import admin
 from django.forms import TextInput, Textarea
 from django.db import models
@@ -194,3 +194,11 @@ class AlertRecipientAdmin(admin.ModelAdmin):
         ('notify_cell', 'cell_number', 'cell_sms_gateway'),
         ('notify_pushover', 'pushover_id')
     )
+
+@admin.register(PeriodicScript)
+class SensorAdmin(admin.ModelAdmin):
+    list_display =  ('script_file_name', 'script_parameters', 'period')
+    readonly_fields = ('script_results',)
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 8, 'cols': 80})},
+    }
