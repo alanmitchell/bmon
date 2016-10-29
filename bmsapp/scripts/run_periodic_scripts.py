bf5305db590b0000cfdba366562ad9f60751612b
Alan Mitchell <tabb99@gmail.com>
2016-10-28 19:32:10 -0800

    Draft Final version of Periodic Scripts module.

--- bmsapp/scripts/run_periodic_scripts.py
+++ bmsapp/scripts/run_periodic_scripts.py
@@ -15,13 +15,17 @@ This script is also called from the main_cron.py script.
 import logging
 import threading
 import time
+from datetime import datetime
 import random
 import importlib
+import traceback
 import yaml
 from bmsapp.readingdb import bmsdata
 import bmsapp.models
 
-CRON_PERIOD = 300
+# This is how often the Periodic scripts are checked to see if it is
+# time to run them.
+CRON_PERIOD = 300    # seconds
 
 def run():
     '''This method is called by the 'runscript' command and is the entry point for
@@ -40,6 +44,7 @@ def run():
             RunScript(script, cron_time).start()
         except:
             logger.exception('Error run %s Periodic Script.' % script.script_file_name)
+
 
 class RunScript(threading.Thread):
     '''
@@ -66,52 +71,68 @@ class RunScript(threading.Thread):
             # Not the correct time to run script, so exit.
             return
 
-        # get a logger.  First strip off any extension from the script
-        # file name and use that as part of the logger name.
-        script_mod_base = self.script.script_file_name.split('.')[0]
-        logger = logging.getLogger('bms.run_periodic_scripts.' + script_mod_base)
-
         # in order to minimize coincident requests on resources due to multiple scripts
         # starting at the same time, introduce a random delay, up to 10 seconds.
         time.sleep(random.random() * 10.0)
 
-        # Assemble the parameter list to pass to the script.  It consists of the
-        # combination of the configuration parameters and saved results from the
-        # last run of the script.
-        params = yaml.load(self.script.script_parameters)
-        params.update(self.script.script_results)
+        try:
+            # Start a dictionary that holds info about this script run and the
+            # results of the script.
+            results ={'script_start_time': datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")}
 
-        # import the periodic script module, but first strip off any extension that
-        # the user may have appended
-        script_mod = importlib.import_module('bmsapp.periodic_scripts.' + script_mod_base)
+            # get a logger.  First strip off any extension from the script
+            # file name and use that as part of the logger name.
+            script_mod_base = self.script.script_file_name.split('.')[0]
+            logger = logging.getLogger('bms.run_periodic_scripts.' + script_mod_base)
 
-        # The script is coded in the 'run' function, so run it with the input parameters.
-        # Capture the output.
-        results = script_mod.run(params)
+            # Assemble the parameter list to pass to the script.  It consists of the
+            # combination of the configuration parameters and saved results from the
+            # last run of the script.  Both of the those sets of parameters are in YAML
+            # form.
+            params = yaml.load(self.script.script_parameters)
+            params.update(yaml.load(self.script.script_results))
 
-        # if the results contains a 'readings' key, then extract those readings for
-        # storage into the reading database.
-        if 'readings' in results:
-            sensor_reads = results.pop('readings')
-            if len(sensor_reads):
-                # change the shape of the sensor readings into separate lists,
-                # which is what the "insert_reading" function call needs.
-                ts, ids, vals = zip(*sensor_reads)
+            # import the periodic script module, but first strip off any extension that
+            # the user may have appended
+            script_mod = importlib.import_module('bmsapp.periodic_scripts.' + script_mod_base)
 
-                # get a connection to the reading database and insert
-                reading_db = bmsdata.BMSdata()
-                insert_msg = reading_db.insert_reading(ts, ids, vals)
-                # store this message so it can be seen in the Django Admin
-                # interface
-                results['reading_insert_message'] = insert_msg
-                logger.debug('Script %s: %s' % (self.script.script_file_name, insert_msg))
-                # **** DO WE WANT TO LOG THIS AS WELL?
+            # The script is coded in the 'run' function, so run it with the input parameters
+            # and record the execution time.
+            start = time.time()
+            script_results = script_mod.run(params)
+            exec_time = time.time() - start
+            results['script_execution_time'] = round(exec_time, 2)
 
+            # if the script returned a dictionary, update the results dictionary with those
+            # values.
+            if type(script_results) == dict:
+                results.update(script_results)
 
-        # Store the results back into the model script object so they are available
-        # for the next call.
-        self.script.script_results = results
-        self.script.script_results.save()
+            # if the results contains a 'readings' key, then extract those readings for
+            # storage into the reading database.
+            if 'readings' in results:
+                sensor_reads = results.pop('readings')
+                if len(sensor_reads):
+                    # change the shape of the sensor readings into separate lists,
+                    # which is what the "insert_reading" function call needs.
+                    ts, ids, vals = zip(*sensor_reads)
 
+                    # get a connection to the reading database and insert
+                    reading_db = bmsdata.BMSdata()
+                    insert_msg = reading_db.insert_reading(ts, ids, vals)
+                    # store this message so it can be seen in the Django Admin
+                    # interface
+                    results['reading_insert_message'] = insert_msg
+                    logger.debug('Script %s: %s' % (self.script.script_file_name, insert_msg))
+
+        except:
+            # record the traceback of the error in the results variable
+            results['script_error'] = traceback.format_exc()
+
+        finally:
+            # Store the results back into the model script object so they are
+            # viewable in the Admin interface and are available for the next call.
+            self.script.script_results = yaml.dump(results)
+            self.script.script_results.save()
 
 
