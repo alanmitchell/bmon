dbf9051528cf09a86b1f6f28c825c9806727d932
Alan Mitchell <tabb99@gmail.com>
2016-11-13 21:07:45 -0900

    Added import to Settings Patch file.
    Makes updating of settings file easier.

--- bmon/settings_patch.py
+++ bmon/settings_patch.py
@@ -1,6 +1,7 @@
 
 # Execute in this namespace the file containing settings that are generally common to all
 # installs of BMON.
+from os.path import dirname, join, abspath
 execfile(join(dirname(abspath(__file__)), 'settings_common.py'))
 
 # If you need to override any of the settings in the 'settings_common.py' file
