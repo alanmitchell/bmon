203bfc56bb45e4e1900874d250a37faeda4fa606
Alan Mitchell <tabb99@gmail.com>
2017-03-29 18:32:26 -0800

    Commented Update script.

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Script to update BMON to the most recent version,
+# applying migrations and copying static files to
+# the static serving application.
 ../apache2/bin/stop
 git pull
 ./manage.py migrate
