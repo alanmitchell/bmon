f8b64f180cc1af678e15a7c03a62530ba4093f26
Alan Mitchell <tabb99@gmail.com>
2017-05-25 14:36:35 -0800

    Replaced tabs with spaces.

--- bmsapp/sms_gateways.py
+++ bmsapp/sms_gateways.py
@@ -5,10 +5,10 @@ model.
 GATEWAYS = (
     ('msg.acsalaska.com', 'Alaska Communications (ACS)'),
     ('txt.att.net', 'AT&T'),
-	('cingular.com', 'Cingular'),
+    ('cingular.com', 'Cingular'),
     ('mobile.gci.net', 'General Communications Inc. (GCI)'),
     ('sms.mtawireless.com', 'MTA Wireless'),
-	('messaging.sprintpcs.com', 'Sprint'),
-	('tmomail.net', 'T-Moble'),
+    ('messaging.sprintpcs.com', 'Sprint'),
+    ('tmomail.net', 'T-Moble'),
     ('vtext.com', 'Verizon Wireless'),
 )
