a1daa842811273cf8252045059b20cc4e38743b5
kthomasak <kthomasak@gmail.com>
2017-05-25 12:34:18 -0800

    added Sprint, T-Mobile, and Cingular cell providers

--- bmsapp/sms_gateways.py
+++ bmsapp/sms_gateways.py
@@ -5,7 +5,10 @@ model.
 GATEWAYS = (
     ('msg.acsalaska.com', 'Alaska Communications (ACS)'),
     ('txt.att.net', 'AT&T'),
+	('cingular.com', 'Cingular'),
     ('mobile.gci.net', 'General Communications Inc. (GCI)'),
     ('sms.mtawireless.com', 'MTA Wireless'),
+	('messaging.sprintpcs.com', 'Sprint'),
+	('tmomail.net', 'T-Moble'),
     ('vtext.com', 'Verizon Wireless'),
 )
