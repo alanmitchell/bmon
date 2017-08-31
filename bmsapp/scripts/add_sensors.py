41d05865ceeff270acaec3d9cd824574746e4984
Alan Mitchell <tabb99@gmail.com>
2017-08-31 13:19:55 -0800

    Changed bulk sensor add script to use Excel Sheet directly.

--- bmsapp/scripts/add_sensors.py
+++ bmsapp/scripts/add_sensors.py
@@ -2,66 +2,76 @@
 a text file.  This can be less time consuming than using the Admin interface
 to add the sensors.
 
-The sensor information must be stored in a text file with the name
-'new_sensors.txt' stored in the subdirectory 'files' beneath this script.  The 
-first line of that file is a header line.  The file must have the format shown 
-in the New_Sensors.xlsx spreadsheet stored in the 'files' subdirectory.
+The sensor information must be stored in an Excel file and the path of that
+file is passed as a command line argument to this script.
+The file must have the format shown in the add_sensors_sample.xlsx spreadsheet
+stored in the 'files' subdirectory.
 
 All SensorGroup and Unit objects must be present before running this script.
 Building objects will be created as needed.  If a sensor with the same
 sensor_id is present in the database, it will be used with all of its existing
 properties instead of creating a new one.
 
-Run this script by using the "runscript" command from the django_extensions
-application.  This application must be listed in the INSTALLED_APPS setting in
-the django settings.py file.  
-See http://django-extensions.readthedocs.org/en/latest/runscript.html for 
-documentation.
-
 Run the script with:
-    ./manage.py runscript add_sensors   # note that there is no '.py' added
+    add_sensors.py excel_file_name.xlsx
 '''
 
-from bmsapp.models import SensorGroup, Unit, Sensor, Building, BldgToSensor
-import os, traceback
+import os
+import sys
+import traceback
+import django
+import pandas as pd
 
-def run():
-    '''This method is called by the 'runscript' command.
-    '''
-    
-    # walk through the lines in the input file, each one being a sensor
-    in_file = os.path.join(os.path.dirname(__file__), 'files/new_sensors.txt')
-    for lin in open(in_file).readlines()[1:]: # skip the header line
-        try:
-            flds = lin.split('\t')
-            
-            # Get the building or make one if not there
-            bldgs = Building.objects.filter(title=flds[0])
-            if len(bldgs):
-                bldg = bldgs[0]
-            else:
-                bldg = Building(title=flds[0])
-                bldg.save()
-    
-            # make the Sensor object if it doesn't exist already
-            sensors = Sensor.objects.filter(sensor_id=flds[1])
-            if len(sensors):
-                sensor = sensors[0]
-            else:
-                sensor = Sensor(sensor_id=flds[1], title=flds[2])
-                sen_unit = Unit.objects.filter(label=flds[3])[0]
-                sensor.unit = sen_unit
-                if len(flds[7].strip()):
-                    sensor.is_calculated = (flds[6].strip()=='TRUE')
-                    sensor.tran_calc_function = flds[7].strip()
-                    sensor.function_parameters = flds[8].strip()
-                sensor.save()
-                
-            # now the object that links sensors to buildings
-            grp = SensorGroup.objects.filter(title=flds[4])[0]
-            link = BldgToSensor(building=bldg, sensor=sensor, sensor_group=grp, sort_order=int(flds[5]))
-            link.save()
-            
-        except:
-            print 'Error processing: %s' % lin
-            traceback.print_exc()
+# need to add the directory two above this to the Python path
+# so the bmsapp package can be found
+this_dir = os.path.dirname(os.path.abspath(__file__))
+app_dir = os.path.abspath(os.path.join(this_dir, '../../'))
+sys.path.insert(0, app_dir)
+
+# prep the Django environment because we need access to the module
+# that manipulates the Reading database.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bmon.settings")
+django.setup()
+
+from bmsapp.models import SensorGroup, Unit, Sensor, Building, BldgToSensor
+
+# Read the Excel file into a DataFrame and delete blank lines
+df = pd.read_excel(sys.argv[1]).dropna(how='all')
+
+for ix, sens in df.iterrows():
+    try:
+
+        # Get the building or make one if not there
+        bldgs = Building.objects.filter(title=sens.Building)
+        if len(bldgs):
+            bldg = bldgs[0]
+        else:
+            bldg = Building(title=sens.Building)
+            bldg.save()
+
+        # make the Sensor object if it doesn't exist already
+        sensors = Sensor.objects.filter(sensor_id=sens['Sensor ID'])
+        if len(sensors):
+            sensor = sensors[0]
+        else:
+            sensor = Sensor(sensor_id=sens['Sensor ID'], title=sens.Title)
+            sen_unit = Unit.objects.filter(label=sens['Unit Label'])[0]
+            sensor.unit = sen_unit
+            if type(sens['Calc or Transform Function'])==unicode:
+                sensor.is_calculated = True if sens['Is Calc Field']==1.0 else False
+                sensor.tran_calc_function = sens['Calc or Transform Function'].strip()
+                try:
+                    sensor.function_parameters = sens['Function Parameters'].strip()
+                except:
+                    # if blank, strip() throws error
+                    sensor.function_parameters = ''
+            sensor.save()
+
+        # now the object that links sensors to buildings
+        grp = SensorGroup.objects.filter(title=sens['Sensor Group'])[0]
+        link = BldgToSensor(building=bldg, sensor=sensor, sensor_group=grp, sort_order=int(sens['Sort Order']))
+        link.save()
+
+    except:
+        print 'Error processing: %s' % sens
+        traceback.print_exc()
