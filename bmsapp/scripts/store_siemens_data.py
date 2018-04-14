ceac05ca808b7fd8af9b5ffffd6346ac09881022
Alan Mitchell <tabb99@gmail.com>
2018-04-13 19:18:47 -0800

    Added actual script that gets called when Email arrives.

---
+++ bmsapp/scripts/store_siemens_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Stores Siemens DDC point data from email messages with trend file
+attachments.
+"""
+import readers.siemens
+import mail2bmon
+
+mail2bmon.process_email(r'.+\.csv$', readers.siemens.parse_file, 'US/Alaska')
