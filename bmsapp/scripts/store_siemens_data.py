f8fe646da80c952c362848ebcf56cab829dd027b
Alan Mitchell <tabb99@gmail.com>
2019-06-10 19:31:14 -0800

    Fixed imports in MEA Siemens script.

--- bmsapp/scripts/store_siemens_data.py
+++ bmsapp/scripts/store_siemens_data.py
@@ -2,7 +2,7 @@
 """Stores Siemens DDC point data from email messages with trend file
 attachments.
 """
-from . import readers.siemens
-from . import mail2bmon
+import readers.siemens
+import mail2bmon
 
 mail2bmon.process_email(r'.+\.csv$', readers.siemens.parse_file, 'US/Alaska')
