63ff3f766899df8676152585377c25a57cd57078
Alan Mitchell <tabb99@gmail.com>
2020-01-29 17:09:05 -0900

    Fixed import problem in "settings_example.py" file.

--- bmon/settings_example.py
+++ bmon/settings_example.py
@@ -1,7 +1,6 @@
 ﻿##################################################
 # Django settings for BMON project               #
 ##################################################
-from .settings_common import *
 import logging
 
 # ----------------- Settings Specific to the Monitoring App ----------------------
@@ -145,6 +144,7 @@ DEBUG = False
 BMSAPP_STATIC_APP_NAME = 'bmon_static'
 
 # Import settings that are generally common to all installs of BMON.
+from .settings_common import *
 
 # ----- If you need to override any of the settings in the 'settings_common.py' file
 # ----- do so below this point in this file.
