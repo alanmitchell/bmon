934dfa9b4b8a1da4f8d79134e79311b59c5836f1
Alan Mitchell <tabb99@gmail.com>
2020-08-20 20:31:56 -0800

    Changed script to terminate old main_cron to use standard library packages.
    Had trouble installing the 3rd party 'psutil' package on Webfaction servers.

--- bmsapp/scripts/terminate_old_cron.py
+++ bmsapp/scripts/terminate_old_cron.py
@@ -5,9 +5,8 @@ time threshhold. Should be run with the django-extensions runscript facility:
 
 This script is also called from the main_cron.py script.    
 '''
-import time
 import getpass
-import psutil
+import subprocess
 
 # Hold a main_cron process must be in seconds before being terminated.
 OLD_SECS = 3600
@@ -18,21 +17,12 @@ def run():
     # determine the User name that is running BMON.
     this_user = getpass.getuser()
 
-    # A list of all the old processes to terminate.
-    to_terminate = []
-
-    for p in psutil.process_iter(['username']):
-        if p.info['username'] == this_user:
-            # Check all the pieces of the command (past the interpreter command in the first
-            # index) to see if the "main_cron" process is part of the name.
-            for fld in p.cmdline()[1:]:
-                if 'main_cron' in fld:
-                    if time.time() - p.create_time() > OLD_SECS:
-                        to_terminate.append(p)
-                    break
-    for p in to_terminate:
-        print(f"Terminating: {' '.join(p.cmdline())}")
-        p.terminate()
-    _, alive = psutil.wait_procs(to_terminate, timeout=3)
-    for p in alive:
-        p.kill()
+    completed = subprocess.run('ps -u $USER -o pid,etimes,command', shell=True, text=True, check=False, stdout=subprocess.PIPE)
+    for proc in completed.stdout.splitlines():
+        if 'runscript main_cron' in proc:
+            flds = proc.split()
+            pid = flds[0].strip()
+            etime = float(flds[1])
+            if etime > OLD_SECS:
+                print(f'Terminating: {pid}')
+                subprocess.run(f'kill {pid}', shell=True, check=False)
