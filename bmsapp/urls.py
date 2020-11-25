aae604cad714e595e194769b26b1af2e78ddb265
alaskamapscience <github@alaskamapscience.com>
2020-11-25 10:36:48 -0900

    Add Sensor Data Utilities page

--- bmsapp/urls.py
+++ bmsapp/urls.py
@@ -10,16 +10,23 @@ from bmsapp import views_api_v2
 
 # Could work on simplifying many of these by using the new "path" function
 urlpatterns = [
-    re_path(r'^readingdb/reading/(\w+)/store/$', views.store_reading),      # URL to store one reading into database
-    re_path(r'^readingdb/reading/store/$', views.store_readings),          # URL to store multiple readings into database
-    re_path(r'^readingdb/reading/store-things/$', views.store_readings_things),  # URL to store readings from Things Network
-    re_path(r'^readingdb/reading/store-rb/$', views.store_readings_radio_bridge),
-    re_path(r'^st8(\w+)/', views.store_reading_old),             # Old URL pattern for storing.  Shouldn't be used for new sensors.
-    re_path(r'^readingdb/reading/(\w+)/$', views.get_readings),   # gets all readings for one reading ID.
+    # URL to store one reading into database
+    re_path(r'^readingdb/reading/(\w+)/store/$', views.store_reading),
+    # URL to store multiple readings into database
+    re_path(r'^readingdb/reading/store/$', views.store_readings),
+    # URL to store readings from Things Network
+    re_path(r'^readingdb/reading/store-things/$', views.store_readings_things),
+    re_path(r'^readingdb/reading/store-rb/$',
+            views.store_readings_radio_bridge),
+    # Old URL pattern for storing.  Shouldn't be used for new sensors.
+    re_path(r'^st8(\w+)/', views.store_reading_old),
+    # gets all readings for one reading ID.
+    re_path(r'^readingdb/reading/(\w+)/$', views.get_readings),
     re_path(r'^$', views.index, name='index'),
     re_path(r'^reports/$', views.reports, name='reports'),
     re_path(r'^reports/results/$', views.get_report_results),
-    re_path(r'^reports/embed/$', views.get_embedded_results), # javascript embedded version of report results
+    # javascript embedded version of report results
+    re_path(r'^reports/embed/$', views.get_embedded_results),
     re_path(r'^energy-reports/$', views.energy_reports),
     re_path(r'^custom-reports/$', views.custom_report_list),
     re_path(r'^custom-reports/(.+)$', views.custom_report),
@@ -29,11 +36,13 @@ urlpatterns = [
     re_path(r'^chart-sensor-list/(\d+)/(multi)/$', views.chart_sensor_list),
     re_path(r'^chart-sensor-list/(\d+)/(\d+)/$', views.chart_sensor_list),
     re_path(r'^map-json/$', views.map_json, name='map-json'),
-    re_path(r'^training/video/(\w+)/(\d+)/(\d+)/$', views.show_video, name='show-video'),
+    re_path(r'^training/video/(\w+)/(\d+)/(\d+)/$',
+            views.show_video, name='show-video'),
     re_path(r'^make-store-key/$', views.make_store_key),
     re_path(r'^ecobee-auth/$', views.ecobee_auth),
     re_path(r'^unassigned-sensors/$', views.unassigned_sensors),
     re_path(r'^backup-readings/$', views.backup_reading_db),
+    re_path(r'^sensor-data-utilities/$', views.sensor_data_utilities),
 
     # Views related to the API, version 1
     re_path(r'^api/v1/version/$', views_api_v1.api_version),
