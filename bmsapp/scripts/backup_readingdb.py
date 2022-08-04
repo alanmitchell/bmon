91a61843aa5602bd9e9c195f626060f73fbfa808
Alan Mitchell <tabb99@gmail.com>
2022-08-04 15:17:43 -0800

    Reduced sensor reading backup retention to 10 days.

--- bmsapp/scripts/backup_readingdb.py
+++ bmsapp/scripts/backup_readingdb.py
@@ -8,7 +8,7 @@ This script is also called from the main_cron.py script.
 """ 
 import bmsapp.readingdb.bmsdata
 
-DAYS_TO_RETAIN = 21   # days of old backup files to retain
+DAYS_TO_RETAIN = 10   # days of old backup files to retain
 
 def run():
     '''Method called by runscript.
