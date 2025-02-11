bbce201d5f160cee7c023c0f97b4c192562260f3
alanmitchell <tabb99@gmail.com>
2025-02-11 10:57:19 -0900

    Raised an error if LoRa Diagnostic connection unsuccessful.

--- bmsapp/lora_diagnostics/db_connect.py
+++ bmsapp/lora_diagnostics/db_connect.py
@@ -9,7 +9,7 @@ db_path = Path(__file__).parent / "db" / "things.db"
 
 def get_rw_db_conn():
     """Returns a read/write connection to the diagnostic database, retrying if needed.
-    Returns None if connection is unsuccessful. Increasing waits between retries, because
+    Raises ConnectionError if unsuccessful. Increasing waits between retries, because
     longer waits are not a problem (no user interaction) for the writes that occur.
     Read/write connections will lock-out other processes, and will lock-out read-only 
     connections in the same process (all connections in the same process must be of the
@@ -26,11 +26,11 @@ def get_rw_db_conn():
                 time.sleep(wait)
                 wait *= 2.0
 
-    return None
+    raise ConnectionError("LoRa Diagnostic Database read/write connection failed.")
 
 def get_read_only_db_conn():
     """Returns a read-only connection to the diagnostic database, retrying if needed.
-    Returns None if unsuccessful. Constant wait, short retries, cuz User is waiting.
+    Raises ConnectionError if unsuccessful. Constant wait, short retries, cuz User is waiting.
     Different processes can have simultaneous read-only connections.
     """
     wait = 0.5     # seconds
@@ -43,4 +43,4 @@ def get_read_only_db_conn():
             if i != retries - 1:
                 time.sleep(wait)
 
-    return None
+    raise ConnectionError("LoRa Diagnostic Database read only connection failed.")
