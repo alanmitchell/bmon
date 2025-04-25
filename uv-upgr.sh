09153f92c06415ff08f9fbec64e1e7b5c816de7b
alanmitchell <tabb99@gmail.com>
2025-04-24 17:00:22 -0800

    Improved migrate script.

--- uv-upgr.sh
+++ uv-upgr.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
 # Commands that need to be run to upgrade to uv
 
 git pull
 sudo systemctl stop bmon
-git checkout uv-package-mgr
+
 rm -rf env/
 uv sync
-# edit crontab
-# edit /etc/systemd/system/bmon.service
+
+.venv/bin/python manage.py migrate
+.venv/bin/python manage.py collectstatic --noinput
+
+echo "Edit Crontab and Edit /etc/systemd/system/bmon.service and then press Enter"
+read
+
 sudo systemctl daemon-reload
 sudo systemctl restart bmon
